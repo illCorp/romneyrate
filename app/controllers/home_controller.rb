6da0e7f7f2d7f53a74034020726bfb47809298cf
szehnder <sean@meeps.com>
2012-11-01 17:17:54 -0600

    removing the test friends

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,13 +11,13 @@ class HomeController < ApplicationController
       current_user = facebook_user
       @friend_likes = facebook_friend_likes
       @friend_likes = [] if (@friend_likes.nil?)
-      foo = []
-      if @friend_likes.count>0
-        100.times do |i|
-          foo << @friend_likes[i%@friend_likes.count]
-        end
-      end
-      @friend_likes = foo
+      #foo = []
+      #if @friend_likes.count>0
+      #  100.times do |i|
+      #    foo << @friend_likes[i%@friend_likes.count]
+      #  end
+      #end
+      #@friend_likes = foo
       @total_friends = facebook_friend_count
       redirect_to :action => :login if current_user.nil?
     end
