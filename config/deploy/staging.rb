7690f6b6d8c12ccbccdef1b51e376206f51c7bbe
szehnder <sean@meeps.com>
2012-11-01 14:00:56 -0600

    getting more stuff in place

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,6 @@ set(:whenever_command) { "RAILS_ENV=#{rails_env} bundle exec whenever" }
 set :branch, "master"
 set :recipients, %w(sean@meeps.com mranauro@meeps.com)
 
-server "ec2-184-73-62-49.compute-1.amazonaws.com", :app, :web, :db, :assets, primary: true
+server "ec2-184-72-92-251.compute-1.amazonaws.com", :app, :web, :db, :assets, primary: true
 #server "ec2-184-72-181-207.compute-1.amazonaws.com", :app, :web, :db, primary: true
 #server "ec2-174-129-131-101.compute-1.amazonaws.com", :app, :web, :db, primary: true
