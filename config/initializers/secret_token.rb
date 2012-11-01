51c1017b7b005b332a408d895005c36ea86cc995
szehnder <sean@meeps.com>
2012-11-01 07:00:34 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Romneyrate::Application.config.secret_token = '29a10d8cac8a839b0f83bd6e136a5fdea575ea7b2fa57d57765a735e977455f65534822606f5653187a24c2f4159b08107dee06d1601d21fce5ca63015ad97fc'
