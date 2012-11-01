8e70bd820a45d247a4d1c278f7082da5e5989873
szehnder <sean@meeps.com>
2012-11-01 17:11:30 -0600

    cosmetic bug fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,14 @@ class HomeController < ApplicationController
     if (facebook_authorized?)
       current_user = facebook_user
       @friend_likes = facebook_friend_likes
+      @friend_likes = [] if (@friend_likes.nil?)
+      foo = []
+      if @friend_likes.count>0
+        100.times do |i|
+          foo << @friend_likes[i%@friend_likes.count]
+        end
+      end
+      @friend_likes = foo
       @total_friends = facebook_friend_count
       redirect_to :action => :login if current_user.nil?
     end
