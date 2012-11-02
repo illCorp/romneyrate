ce0a6447840e45f5bf08f2463d697717b143f869
szehnder <sean@meeps.com>
2012-11-02 11:36:20 -0600

    illco shit

--- app/controllers/illco_controller.rb
+++ app/controllers/illco_controller.rb
@@ -1,6 +1,6 @@
 class IllcoController < ApplicationController
   # http_basic_authenticate_with name: "meeps", password: "@g3rr3ds3cur3!", except: :index
   def index
-    render :layout => nil
+    render :layout => 'illco'
   end
 end
